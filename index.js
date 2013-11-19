27c21d4099836567eb743c177e75b1a5bdbfb661
David Guttman <david@davidguttman.com>
2013-11-19 07:04:13 -0800

    will now open url after deploy

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 
 var request = require('request')
+var open = require('open')
 
 var ws = request.post('http://hedwig.in/create', function(err, res, body) {
   if (err) return console.error(err)
 
   var url = 'http://hedwig.in/' + body
-  console.log(url)
+  console.log('Deployed to', url)
+  open(url)
 })
 
 process.stdin.pipe(ws)
