1f863d0e15506c78d84bcbbc3bd9cb8b84844eac
David Guttman <david@davidguttman.com>
2013-11-19 07:17:55 -0800

    now minified with uglify

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
 var request = require('request')
+var through = require('through')
 var open = require('open')
+var ujs = require('uglify-js')
 
 var ws = request.post('http://hedwig.in/create', function(err, res, body) {
   if (err) return console.error(err)
@@ -11,4 +13,18 @@ var ws = request.post('http://hedwig.in/create', function(err, res, body) {
   open(url)
 })
 
-process.stdin.pipe(ws)
+var buffer = ''
+var ugs = through(function onData (data) {
+  buffer += data
+}, function onEnd () {
+  var minned = ujs.minify(buffer, {
+      fromString: true
+    , compress: true
+    , mangle: true
+  })
+
+  this.queue(minned.code)
+  this.queue(null)
+})
+
+process.stdin.pipe(ugs).pipe(ws)
