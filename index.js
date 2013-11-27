339797fa0fb8cb3ee1748a76ad1bd61468b03f26
David Guttman <david@davidguttman.com>
2013-11-27 12:50:17 -0800

    post bundles to github instead of hedwig.in

--- index.js
+++ index.js
@@ -3,18 +3,17 @@
 var request = require('request')
 var through = require('through')
 var open = require('open')
-var zlib = require('zlib')
 var ujs = require('uglify-js')
 
 var baseUrl = 'http://hedwig.in/'
 
-var ws = request.post(baseUrl + 'create', function(err, res, body) {
-  if (err) return console.error(err)
+// var ws = request.post(baseUrl + 'create', function(err, res, body) {
+//   if (err) return console.error(err)
 
-  var url = baseUrl + body
-  console.log('Deployed to', url)
-  open(url)
-})
+//   var url = baseUrl + body
+//   console.log('Deployed to', url)
+//   open(url)
+// })
 
 var buffer = ''
 var ugs = through(function onData (data) {
@@ -30,4 +29,50 @@ var ugs = through(function onData (data) {
   this.queue(null)
 })
 
-process.stdin.pipe(ugs).pipe(zlib.createGzip()).pipe(ws)
+process.stdin.pipe(ugs)
+
+ugs.on('data', function(minned) {
+  createGist(minned, function(err, id) {
+    if (err) return console.error(err)
+
+    var url = baseUrl + 'g/' + id
+    console.log('Deployed to', url)
+    open(url)
+  })
+})
+
+function createGist (code, cb) {
+  var opts = createGistOpts(code)
+  request(opts, function(err, res, body) {
+    if (err) return cb(err)
+
+    if (res.statusCode === 201) {
+      var id = res.headers.location.split('github.com/gists/')[1]
+      return cb(null, id)
+    } 
+    
+    console.error('Error creating gist', res.headers.status)
+    cb(new Error(res.headers.status))
+  });
+}
+
+function createGistOpts (code) {
+  var opts = {
+    url: 'https://api.github.com/gists',
+    method: 'POST',
+    json: {
+      description: 'hedwig bundle',
+      "public": true,
+      files: {
+        'minified.js': {
+          content: code
+        }
+      }
+    },
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  return opts
+}
